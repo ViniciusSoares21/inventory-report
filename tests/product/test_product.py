db2b46f5f1b2a24bd3ce55a11bf99c00b496fde6
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-04 15:41:10 -0300

    test: criação de produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product1 = Product(1, 'site', 'Vjcompany', '2023-04-04', '2025-04-04',
+                       '0001', 'Git Hub')
+    assert product1.id == 1
+    assert product1.nome_da_empresa == 'Vjcompany'
+    assert product1.nome_do_produto == 'site'
+    assert product1.data_de_fabricacao == '2023-04-04'
+    assert product1.data_de_validade == '2025-04-04'
+    assert product1.numero_de_serie == '0001'
+    assert product1.instrucoes_de_armazenamento == 'Git Hub'
