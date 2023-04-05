495f562fcb623f262ed0605f7629b4fe9446704d
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-05 18:22:45 -0300

    test: para retoeno da class product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,14 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product1 = Product(
+        1, "site", "Vjcompany", "2023-04-04", "2025-04-04", "0001", "Git Hub"
+    )
+
+    product_result = str(product1)
+
+    string_return = """O produto site fabricado em 2023-04-04 por Vjcompany \
+com validade até 2025-04-04 precisa ser armazenado Git Hub."""
+
+    assert string_return == product_result
