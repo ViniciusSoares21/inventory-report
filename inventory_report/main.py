740455d10ae356f1761539b2d308471a588baff2
Vinicius Soares <viniciussoares72943@gmail.com>
2023-04-10 14:38:23 -0300

    feat: funcionalidade para o usuario poder pedir um relatorio pelo terminal

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -4,22 +4,19 @@ from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.xml_importer import XmlImporter
 
-arguments = sys.argv
-
 
 def main():
-    if len(arguments) < 3:
+    if len(sys.argv) < 3:
         print("Verifique os argumentos", file=sys.stderr)
         return None
 
-    if '.csv' in arguments[1]:
+    if '.csv' in sys.argv[1]:
         report = InventoryRefactor(CsvImporter)
-    if '.json' in arguments[1]:
+    if '.json' in sys.argv[1]:
         report = InventoryRefactor(JsonImporter)
-    if '.xml' in arguments[1]:
+    if '.xml' in sys.argv[1]:
         report = InventoryRefactor(XmlImporter)
 
-    return print(report.import_data(
-        arguments[1], arguments[2]),
-        file=sys.stdout
+    return sys.stdout.write(report.import_data(
+        sys.argv[1], sys.argv[2])
     )
